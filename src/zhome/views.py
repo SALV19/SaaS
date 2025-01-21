89152d8474b18f5bcb02171e7537c21cafc135a9
SALV <a01707122@tec.mx>
2025-01-21 12:16:59 -0600

    Docker / Gunicorn / railway setup

--- src/zhome/views.py
+++ src/zhome/views.py
@@ -7,10 +7,16 @@ this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
   queryset = PageVisits.objects.filter(path=request.path)
+  all_visits = PageVisits.objects.all()
+  try:
+    percent = queryset.count() * 100 / all_visits
+  except:
+    percent = 0
   my_title="Indiana Jones"
   my_context= {
     "page_title": my_title,
-    "page_visits_count": queryset.count()
+    "page_visits_count": queryset.count(),
+    "percentage-visits": percent
   }
   path = request.path
   html_template ="home.html"

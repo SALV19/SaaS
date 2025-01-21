e7b924e10ce76d34335a1ff2c35797c9ba122a06
SALV <a01707122@tec.mx>
2025-01-21 12:44:22 -0600

    [ADD] env

--- src/zhome/settings.py
+++ src/zhome/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-be5cle!4%#8_o+^+&a=au##0=nu)bjh@n$!fp0y+!#t5yy-$)d'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = [
     ".railway.app" # http://saas.prod.railway.app
